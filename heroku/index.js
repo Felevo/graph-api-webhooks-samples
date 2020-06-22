247820562d98c24841de5c37a477473a77087d1d
Felix Jan Schneider <43845422+Felevo@users.noreply.github.com>
2020-06-22 13:34:30 +0200

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -27,6 +27,7 @@ app.get("/", function (req, res) {
 });
 
 app.get(["/facebook", "/instagram"], function (req, res) {
+  received_updates.push(JSON.stringify(req));
   if (
     req.query["hub.mode"] == "subscribe" &&
     req.query["hub.verify_token"] == token
